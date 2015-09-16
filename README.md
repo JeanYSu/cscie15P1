0d9fd52827be2adc5e219b8493db94ba83a23a8d
JeanYSu <ysu@g.hardvard.edu>
2015-09-16 00:17:19 -0700

    add images, update index.php with completed content for P1

---
+++ README.md
@@ -0,0 +1,22 @@
+# CSCI E-15 Project 1 by Jean Yu Chun Su
+
+## Live URL
+[http://p1.meiosise.com](http://p1.meiosise.com)
+
+## Description
+A project/personal info portfolio page was set up to present all the projects that Jean will complete in CSCI E-15 course.
+Git, Github and Digital Ocean server were applied in the project.
+HTML and CSS skills were demonstrated with Bootstrap technology
+
+## Demo
+Not applicable in this project.
+
+## Details for teaching team
+No login required.
+
+The file `index.php` was used to generate a portfol
+
+## Outside code
+* Word list: http://www.paulnoll.com/Books/Clear-English/
+* Bootstrap: http://getbootstrap.com/
+* Bootstrap Theme: http://bootswatch.com/flatly/
--- index.php
+++ index.php
@@ -1,20 +1,75 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>CSCI E-15 P1 Home Page</title>
-	<meta charset='utf-8'>
+
+	<title>Jean Yu Chun Su P1 - DWA15 Portfolio</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  	<style>
+		h1{
+			text-align:center;}
+		img.img-rounded{
+			display: block;
+			margin-left: auto;
+			margin-right: auto}
+	</style>
 </head>
 <body>
+<div class="container">
+	<div class="jumbotron">
+		<h1>Jean Yu Chun Su </h1>
+		<p style="text-align:center"><b>CSCI E-15 Dynamic Web Application Portfolio</b></p>
+		<img src='./images/Jean.jpg'  class="img-rounded" alt='Jean Su' width="280" height="300">
+	</div>
+	<div class="row">
+		<div class="col-sm-4">
+			 <h2>Project 2 Title</h2>
+			 		<p>View: <a href="http://p2.meiosise.com">[http://p2.meiosise.com]</a></p>
+					<p>Github: <a href="https://github.com/JeanYSu/cscie15P2">[cscie15P2]</a></p>
+		</div>
+		<div class="col-sm-4">
+			 <h2>Project 3 Title</h2>
+					<p>View: <a href="http://p3.meiosise.com">[http://p3.meiosise.com]</a></p>
+					<p>Github: <a href="https://github.com/JeanYSu/cscie15P3">[cscie15P3]</a></p>
+		</div>
+		<div class="col-sm-4">
+			 <h2>Project 4 Title</h2>
+					<p>View: <a href="http://p4.meiosise.com">[http://p4.meiosise.com]</a></p>
+					<p>Github: <a href="https://github.com/JeanYSu/cscie15P4">[cscie15P4]</a></p>
+		</div>
+	</div>
 
-	<?php
-	echo 'Porject #: Project 1 <br>';
-	echo 'Project Member: Jean<br>';
-	echo 'Server Name: ';
-	echo $_SERVER['SERVER_NAME'];
-	?>
-	<br>
-	<br>
-	<br>
-	<h1>Under Construction</h1>
+		<h3>In 2+ sentences describe who you are and what you do.</h3>
+		<p>I am a working mom with a lovely son born last April.
+			Originally I came from Taiwan and moved to Vancouver Canada in 1995 for my university study.
+			I have been a software engineer on VB, ASP and ASP.NET over last 14 years and then moved into development management role for last 10 years.
+			I have managed two offline development centers in China until end of 2014 before I opened my own software engineering company in Vancouver, BC Canada.
+			Now I run a startup site <a href="http://www.ClassEntry.com">ClassEntry.com</a> with my two other co-founders to help parents find great private teachers nearby their homes.
+		</p>
+		<h3>Why are you taking this course?</h3>
+		<p>
+			I am taking this course for my master degree program on Information System.
+			In addition, I wish to learn and use more PHP technology linked to Facebook developer toolkits in my side projects at ClassEntry.com.
+		</p>
+		<h3>What previous experience do you have with programming?</h3>
+		<p>
+			I have been programming with VB, ASP and ASP.NET daily at my job before 2008.
+			I still keep coding work occasionally with my team and most of time I did code reviews with junior developers in the team.
+		</p>
+		<h3>Are you working on a Mac, PC, or some other platform?</h3>
+		<p>I am working on a Mac OSX.
+		</p>
+		<h3>Anything else we should know?</h3>
+		<p>
+			I would like to learn a lot more about FB integration API with PHP in this course if possible.
+		</p>
+		<br>
+	</div>
+
+
 </body>
 </html>
