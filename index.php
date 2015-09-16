4222f39418284a31a83a8c251786726c9626149b
JeanYSu <ysu@g.hardvard.edu>
2015-09-15 17:34:48 -0700

    add index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>P1 Home Page</title>
+	<meta charset='utf-8'>
+
+</head>
+<body>
+
+	<?php
+	echo 'Project 1';
+	echo $_SERVER['SERVER_NAME'];
+	?>
+	<h1>Under Construction</h1>
+</body>
+</html>
